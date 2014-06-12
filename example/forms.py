071780c4d3431ba689da830c3206ab70d684f98d
Matteo Nastasi <nastasi@openquake.org>
2014-06-12 10:31:39 +0200

    changed example to rise reset of all inheriting dropdown menu when the save fails (us add new and than keep the 'name' field empty

--- example/forms.py
+++ example/forms.py
@@ -1,6 +1,6 @@
 from chained_selectbox.forms import ChainedChoicesForm
 from django import forms
-from example.models import StandardModel, StandardModelTwo
+from example.models import StandardModel
 from chained_selectbox.form_fields import ChainedChoiceField
 
 
@@ -8,14 +8,7 @@ class StandardModelForm(ChainedChoicesForm):
     class Meta:
         model = StandardModel
 
-    field_one = forms.ChoiceField(choices=(('', '------------'), (1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'), ))
-    field_two = ChainedChoiceField(parent_field='field_one', ajax_url='/chainedselectchoices')
-    field_three = ChainedChoiceField(parent_field='field_two', ajax_url='/chainedselectchoices')
-
-
-class StandardModelTwoForm(ChainedChoicesForm):
-    class Meta:
-        model = StandardModelTwo
+    name = forms.CharField(max_length=255)
 
     field_one = forms.ChoiceField(choices=(('', '------------'), (1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'), ))
     field_two = ChainedChoiceField(parent_field='field_one', ajax_url='/chainedselectchoices')
