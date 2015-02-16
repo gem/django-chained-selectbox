98f7373c4f099f9725d0f6281db6913cd9247b1c
Matteo Nastasi <nastasi@openquake.org>
2015-02-16 15:29:03 +0100

    typo fixed

--- example/forms.py
+++ example/forms.py
@@ -10,6 +10,6 @@ class CForm(ChainedChoicesForm):
 
     name = forms.CharField(label="name", max_length=255)
 
-    field_one = forms.ChoiceField(label="filed one", choices=(('', '------------'), (1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'), ))
-    field_two = ChainedChoiceField(label="filed two", parent_field='field_one', ajax_url='/chainedselectchoices')
-    field_three = ChainedChoiceField(label="filed three", parent_field='field_two', ajax_url='/chainedselectchoices')
+    field_one = forms.ChoiceField(label="field one", choices=(('', '------------'), (1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'), ))
+    field_two = ChainedChoiceField(label="field two", parent_field='field_one', ajax_url='/chainedselectchoices')
+    field_three = ChainedChoiceField(label="field three", parent_field='field_two', ajax_url='/chainedselectchoices')
